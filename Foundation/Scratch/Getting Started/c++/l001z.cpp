1daf0a12e1056764166fce3ea9c2af7eeaa4df20
Japneet Kaur Bhatia <56752427+japneetbhatia@users.noreply.github.com>
2021-01-01 23:10:45 +0530

    Create l001z.cpp

---
+++ Foundation/Scratch/Getting Started/c++/l001z.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+void printz()
+{
+    cout<<"*****"<<endl;
+    cout<<"   *"<<endl;
+    cout<<"  *"<<endl;
+    cout<<" *"<<endl;
+    cout<<"*****"<<endl;
+}
+int main()
+{
+    printz();
+}

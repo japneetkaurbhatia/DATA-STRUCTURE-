44517811d5ebb8a7ee4dbbe37d94fc697f6a47d2
Japneet Kaur Bhatia <56752427+japneetbhatia@users.noreply.github.com>
2021-01-04 17:56:00 +0530

    Create l001z.java

---
+++ Foundation/Scratch/Getting Started/java/l001z.java
@@ -0,0 +1,14 @@
+public class l001z{
+    public static void printz()
+    {
+        System.out.println("*****");
+        System.out.println("   *");
+        System.out.println("  *");
+        System.out.println(" *");
+        System.out.println("*****");
+    }
+    public static void main(String[] args)
+    {
+        printz();
+    }
+}

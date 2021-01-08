19b2aa169f23f5a3c90d6768c317896cb495b441
Japneet Kaur Bhatia <56752427+japneetbhatia@users.noreply.github.com>
2021-01-08 23:02:39 +0530

    Create l003BenjiumBulbs.java

---
+++ Foundation/Scratch/Getting Started/java/l003BenjiumBulbs.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+public static Scanner scn = new Scanner(System.in);
+    public static void main(String[] args) {
+        // write your code here 
+        int n = scn.nextInt();
+        for(int i = 1; i*i<=n;i++){
+        System.out.println(i*i);
+    }
+    }
+}

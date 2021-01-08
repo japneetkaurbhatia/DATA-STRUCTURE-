a3b4c654d7e9aa03b256bc9063daf1066851e3cc
Japneet Kaur Bhatia <56752427+japneetbhatia@users.noreply.github.com>
2021-01-08 23:05:27 +0530

    Create l003inverseNumber.java

---
+++ Foundation/Scratch/Getting Started/java/l003inverseNumber.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.lang.Math; 
+public class Main {
+    public static Scanner scn = new Scanner(System.in);
+   public static int countDigit(int n){
+    int ctr=0,rem;
+    while(n>0){
+        rem = n%10;
+        ctr++;
+        n /= 10;
+    }
+    return ctr;
+}
+
+public static int inverse(int n){
+    int len = countDigit(n);
+    int ans = 0;
+    for(int i = 1; i <= len; i++){
+        int r = n%10;
+        n /= 10;
+        ans += i*Math.pow(10,(r-1));
+    }
+    return ans;
+}
+
+    public static void main(String[] args) {
+        // write your code here  
+        int n = scn.nextInt();
+        System.out.print(inverse(n));
+    }
+}

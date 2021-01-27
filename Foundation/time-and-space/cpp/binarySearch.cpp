35a79152f4fa24032b19a61caf2f1ab452f52a2e
Japneet Kaur Bhatia <56752427+japneetbhatia@users.noreply.github.com>
2021-01-27 04:30:38 -0800

    Create binarySearch.cpp

---
+++ Foundation/time-and-space/cpp/binarySearch.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+void input(vector<int>& arr){
+    int n = arr.size();
+    for(int i = 0; i<n; i++){
+        cin>>arr[i];
+    }
+}
+int closestElement(vector<int>& arr,int data, int si,int ei){
+    while(si<=ei){
+        int mid = (si+ei)/2;
+        if(arr[mid]==data) return mid;
+        else if(arr[mid]<data)  si = mid+1;
+        else ei = mid-1;
+    }
+    return -1 ;
+}
+int main(){
+    int n; cin>>n;
+    vector<int> A(n,0);
+    input(A);
+    int data; cin>>data;
+    cout<<closestElement(A,data,0,n-1);
+    return 0;
+}

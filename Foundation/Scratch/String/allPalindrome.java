ecc0b89d86da6efccfb9f950cc61df87f83397c4
Japneet Kaur Bhatia <56752427+japneetbhatia@users.noreply.github.com>
2021-02-20 02:30:40 +0530

    Create allPalindrome.java

---
+++ Foundation/Scratch/String/allPalindrome.java
@@ -0,0 +1,8 @@
+public static boolean palindrome(String str){
+        int i = 0,j = str.length()-1;
+        while(i < j){
+            if(str.charAt(i++) != str.charAt(j--))
+               return false;
+        }
+        return true;
+    }

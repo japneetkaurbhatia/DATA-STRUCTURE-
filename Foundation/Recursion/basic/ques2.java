0256b197f6c4f44d0afb904a35cb0c27f9f5dffc
Japneet Kaur Bhatia <56752427+japneetbhatia@users.noreply.github.com>
2021-02-22 23:41:01 +0530

    Create ques2.java

---
+++ Foundation/Recursion/basic/ques2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class ques2{
+    public static Scanner scn = new Scanner(System.in);
+    public static void printDecreasing(int n){
+        if(n==0)   return;
+        System.out.println("hi" + n);
+        printDecreasing(n-1);
+        printDecreasing(n-2);
+        System.out.println("bye" + n);
+    }
+    public static void main(String[] args){
+        int n = scn.nextInt();
+        printDecreasing(n);
+    }
+}

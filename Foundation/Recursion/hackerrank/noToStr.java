fc44f516c9a8f4ce651ad222d52e6361feffdc20
Japneet Kaur Bhatia <56752427+japneetbhatia@users.noreply.github.com>
2021-02-27 22:48:57 +0530

    Create noToStr.java

---
+++ Foundation/Recursion/hackerrank/noToStr.java
@@ -0,0 +1,21 @@
+//https://www.hackerrank.com/contests/pepdec62017/challenges/pep-java-7recursion-17numbertostring/copy-from/1330961077
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    
+    public static long noToStr(String str,int idx,long pow){
+        if(idx == -1) return 0;
+        long recAns = noToStr(str,idx-1,pow * 10);
+        return recAns + (str.charAt(idx) - '0') * pow;
+    } 
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scn = new Scanner(System.in);
+        String str = scn.next();
+        int div = scn.nextInt();
+        System.out.println(noToStr(str,str.length()-1,1)/div);
+    }
+}

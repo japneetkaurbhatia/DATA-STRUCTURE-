a059009b3acb44f4150a9bd9a0ad84cbeacf771e
Japneet Kaur Bhatia <56752427+japneetbhatia@users.noreply.github.com>
2021-03-01 22:48:51 +0530

    Create portalGetVal.java

---
+++ Foundation/LinkedList/portalGetVal.java
@@ -0,0 +1,12 @@
+ public int getFirst(){
+      // write your code here
+      if(this.size == 0)  return -1;
+      return this.head.data;
+      
+    }
+
+    public int getLast(){
+      // write your code here
+      if(this.size == 0)  return -1;
+      return this.tail.data;
+    }

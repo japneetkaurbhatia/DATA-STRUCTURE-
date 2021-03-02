27c4581b8e367820290412416d0b25290bdcd028
Japneet Kaur Bhatia <56752427+japneetbhatia@users.noreply.github.com>
2021-03-02 23:21:31 +0530

    Update portalMidOfLL.java

--- Foundation/LinkedList/portalMidOfLL.java
+++ Foundation/LinkedList/portalMidOfLL.java
@@ -1,6 +1,4 @@
-
- 
-    public int mid(){
+public int mid(){
       // write your code here
       Node slow = head;
       Node fast = head;
@@ -10,62 +8,3 @@
       }
       return slow.data;
     }
-  }
-
-  public static void main(String[] args) throws Exception {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    LinkedList list = new LinkedList();
-
-    String str = br.readLine();
-    while (str.equals("quit") == false) {
-      if (str.startsWith("addLast")) {
-        int val = Integer.parseInt(str.split(" ")[1]);
-        list.addLast(val);
-      } else if (str.startsWith("size")) {
-        System.out.println(list.size());
-      } else if (str.startsWith("display")) {
-        list.display();
-      } else if (str.startsWith("removeFirst")) {
-        list.removeFirst();
-      } else if (str.startsWith("getFirst")) {
-        int val = list.getFirst();
-        if (val != -1) {
-          System.out.println(val);
-        }
-      } else if (str.startsWith("getLast")) {
-        int val = list.getLast();
-        if (val != -1) {
-          System.out.println(val);
-        }
-      } else if (str.startsWith("getAt")) {
-        int idx = Integer.parseInt(str.split(" ")[1]);
-        int val = list.getAt(idx);
-        if (val != -1) {
-          System.out.println(val);
-        }
-      } else if (str.startsWith("addFirst")) {
-        int val = Integer.parseInt(str.split(" ")[1]);
-        list.addFirst(val);
-      } else if (str.startsWith("addAt")) {
-        int idx = Integer.parseInt(str.split(" ")[1]);
-        int val = Integer.parseInt(str.split(" ")[2]);
-        list.addAt(idx, val);
-      } else if (str.startsWith("removeLast")) {
-        list.removeLast();
-      } else if (str.startsWith("removeAt")) {
-        int idx = Integer.parseInt(str.split(" ")[1]);
-        list.removeAt(idx);
-      } else if(str.startsWith("reverseDI")){
-        list.reverseDI();
-      } else if(str.startsWith("reversePI")){
-        list.reversePI();
-      } else if(str.startsWith("kthFromEnd")){
-        int idx = Integer.parseInt(str.split(" ")[1]);
-        System.out.println(list.kthFromLast(idx));
-      } else if(str.startsWith("mid")){
-        System.out.println(list.mid());
-      }
-      str = br.readLine();
-    }
-  }
-}

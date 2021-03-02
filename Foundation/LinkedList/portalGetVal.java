ebc55cc6c4409e2ccd8dc140e668c3e00973c2bd
Japneet Kaur Bhatia <56752427+japneetbhatia@users.noreply.github.com>
2021-03-02 23:18:58 +0530

    Update portalGetVal.java

--- Foundation/LinkedList/portalGetVal.java
+++ Foundation/LinkedList/portalGetVal.java
@@ -1,12 +1,35 @@
  public int getFirst(){
       // write your code here
-      if(this.size == 0)  return -1;
+      if(this.size == 0)  {
+          System.out.println("List is empty");
+          return -1;
+      }
       return this.head.data;
       
     }
 
     public int getLast(){
       // write your code here
-      if(this.size == 0)  return -1;
+      if(this.size == 0)  {
+          System.out.println("List is empty");
+          return -1;
+      }
       return this.tail.data;
+    }
+
+    public int getAt(int idx){
+      // write your code here
+       if (this.size == 0) {
+        System.out.println("List is empty");
+        return -1;
+      } else if (idx < 0 || idx >= size) {
+        System.out.println("Invalid arguments");
+        return -1;
+      } else {
+        Node temp = head;
+        for (int i = 0; i < idx ; i++) {
+          temp = temp.next;
+        }
+        return temp.data;
+      }
     }

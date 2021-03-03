a81ecb8dcb02ee3a30696a78befae6f08faeaf72
Japneet Kaur Bhatia <56752427+japneetbhatia@users.noreply.github.com>
2021-03-03 23:45:53 +0530

    Add files via upload

---
+++ Foundation/tree/portalPrintSingleChild.java
@@ -0,0 +1,10 @@
+public static void printSingleChildNodes(Node node, Node parent) {
+        if (node == null)
+            return;
+        if (parent != null && (parent.left == null || parent.right == null)) {
+            System.out.println(node.data);
+        }
+
+        printSingleChildNodes(node.left, node);
+        printSingleChildNodes(node.right, node);
+    }

5adac974e4a73c20f379219cc2c39f52a5bdd667
Japneet Kaur Bhatia <56752427+japneetbhatia@users.noreply.github.com>
2021-03-19 23:06:21 +0530

    Create linearize.java

---
+++ Foundation/GT/linearize.java
@@ -0,0 +1,16 @@
+public static Node getTail(Node node){
+      Node curr = node;
+      while(curr.children.size() != 0){
+          curr = curr.children.get(0);
+      }
+      return curr;
+  }
+  public static void linearize(Node node){
+    for(Node child : node.children)
+       linearize(child);
+    for(int i = node.children.size()-2; i >= 0; i--){
+        Node tail = getTail(node.children.get(i));
+        tail.children.add(node.children.get(i+1));
+        node.children.remove(i+1);
+    }
+  }
